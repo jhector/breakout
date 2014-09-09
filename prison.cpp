f2c43365a00eaec185e6ce88c71ad54d3a3c6c19
Julian Hector <jhector@mozilla.com>
2014-09-09 15:58:43 +0200

    added basic interaction shell

--- prison.cpp
+++ prison.cpp
@@ -2,18 +2,35 @@
 #include <cstdlib>
 
 #include <stdint.h>
+#include <string.h>
+#include <unistd.h>
 
-void test()
+void interact()
 {
-    throw 1;
+    char cmd[8] = {0};
+
+    write(1, "> ", 2);
+
+    read(0, cmd, sizeof(cmd)-1);
+
+    if (!strncmp(cmd, "exit", 4)) {
+        throw 2;
+    } else {
+        throw 1;
+    }
 }
 
 int32_t main()
 {
-    try {
-        test();
-    } catch (int32_t e) {
-        printf("Excpetion: %d\n", e);
+    while (1) {
+        try {
+            interact();
+        } catch (int32_t e) {
+            if (e == 1)
+                puts("unknown command");
+            else if (e == 2)
+                break;
+        }
     }
 
     return 0;
